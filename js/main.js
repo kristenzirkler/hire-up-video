a632ba3f3d49f36bb0a5d37a503df6b48deec654
Kristen Zirkler <bazirkler@gmail.com>
2016-10-11 09:57:49 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(function(){
     event.preventDefault();
     var videoID = $(this).attr('data-videoID');
    
-    $('#videoPlayerContainer iframe').attr('src','//www.youtube.com/embed/'+videoID+'?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0');
+    $('#videoPlayerContainer iframe').attr('src','https://player.vimeo.com/video/'+videoID+'?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0');
     $('#videoPlayerContainer').show('slow');
     
   });
@@ -37,5 +37,25 @@ $(function(){
     $('#videoPlayerContainer iframe').attr('src','');
   });
 
+  if($('#oneVideoSample').length != 0) {
+    $('#oneVideoText div').hide();
+    var thisVideoID = getParameterByName('vid');
+    $('#oneVideoSample iframe').attr('src','https://player.vimeo.com/video/'+thisVideoID+'?autohide=1&modestbranding=1&rel=0&showinfo=0');
+    //$('#oneVideoSample iframe').attr('src','https://player.vimeo.com/video/'+thisVideoID+'?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0');
+    $('#v-'+thisVideoID).show();
+    $('#oneVideoText').show();
+    $('#oneVideoSample').show();
+    
+  }
+  // Get params in url
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 
 });
